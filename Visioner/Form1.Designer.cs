3819177c078871aa4507614ff29860c066104d27
September <sergey_meshkov.ru@mail.ru>
2023-05-09 17:42:42 +0300

    [NF] Added the change of language, bugs fixed

--- Visioner/Form1.Designer.cs
+++ Visioner/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Visioner
+﻿using Visioner.Dictionary;
+
+namespace Visioner
 {
     partial class Form1
     {
@@ -30,154 +32,173 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.bSpravka = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.bAdd = new System.Windows.Forms.Button();
-            this.bDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.bPredict = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.русскийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(25, 69);
+            this.progressBar1.Location = new System.Drawing.Point(30, 78);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(349, 23);
             this.progressBar1.TabIndex = 1;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label1.Location = new System.Drawing.Point(12, 109);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(374, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Нажми кнопку и я предскажу твоё будущее";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox1.Location = new System.Drawing.Point(25, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(349, 23);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // bSpravka
-            // 
-            this.bSpravka.Location = new System.Drawing.Point(52, 141);
-            this.bSpravka.Name = "bSpravka";
-            this.bSpravka.Size = new System.Drawing.Size(75, 23);
-            this.bSpravka.TabIndex = 4;
-            this.bSpravka.Text = "Справка";
-            this.bSpravka.UseVisualStyleBackColor = true;
-            this.bSpravka.Click += new System.EventHandler(this.buttonSpravka_Click);
-            // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(55, 65);
+            this.listBox1.Location = new System.Drawing.Point(41, 44);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(228, 121);
+            this.listBox1.Size = new System.Drawing.Size(236, 108);
             this.listBox1.TabIndex = 7;
             // 
-            // bAdd
+            // buttonAdd
             // 
-            this.bAdd.Location = new System.Drawing.Point(339, 25);
-            this.bAdd.Name = "bAdd";
-            this.bAdd.Size = new System.Drawing.Size(75, 23);
-            this.bAdd.TabIndex = 8;
-            this.bAdd.Text = "Добавить";
-            this.bAdd.UseVisualStyleBackColor = true;
-            this.bAdd.Click += new System.EventHandler(this.button2_Click);
+            this.buttonAdd.Location = new System.Drawing.Point(322, 15);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
             // 
-            // bDelete
+            // buttonDelete
             // 
-            this.bDelete.Location = new System.Drawing.Point(339, 81);
-            this.bDelete.Name = "bDelete";
-            this.bDelete.Size = new System.Drawing.Size(75, 23);
-            this.bDelete.TabIndex = 9;
-            this.bDelete.Text = "Удалить";
-            this.bDelete.UseVisualStyleBackColor = true;
-            this.bDelete.Click += new System.EventHandler(this.button3_Click);
+            this.buttonDelete.Location = new System.Drawing.Point(322, 54);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
             // 
             // textBox2
             // 
             this.textBox2.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox2.Location = new System.Drawing.Point(18, 25);
+            this.textBox2.Location = new System.Drawing.Point(19, 15);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(300, 23);
+            this.textBox2.Size = new System.Drawing.Size(288, 23);
             this.textBox2.TabIndex = 10;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // tabControl1
             // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, -2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(418, 235);
+            this.tabControl1.Size = new System.Drawing.Size(417, 186);
             this.tabControl1.TabIndex = 11;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.progressBar1);
             this.tabPage1.Controls.Add(this.bPredict);
-            this.tabPage1.Controls.Add(this.bSpravka);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(410, 209);
+            this.tabPage1.Size = new System.Drawing.Size(409, 160);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(30, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 51);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // bPredict
             // 
-            this.bPredict.Location = new System.Drawing.Point(165, 141);
+            this.bPredict.Location = new System.Drawing.Point(154, 116);
             this.bPredict.Name = "bPredict";
-            this.bPredict.Size = new System.Drawing.Size(93, 23);
+            this.bPredict.Size = new System.Drawing.Size(97, 27);
             this.bPredict.TabIndex = 0;
-            this.bPredict.Text = "КНОПКА";
             this.bPredict.UseVisualStyleBackColor = true;
-            this.bPredict.Click += new System.EventHandler(this.bPredict_Click);
+            this.bPredict.Click += new System.EventHandler(this.ButtonPredict_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.listBox1);
             this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Controls.Add(this.bDelete);
-            this.tabPage2.Controls.Add(this.bAdd);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Controls.Add(this.buttonDelete);
+            this.tabPage2.Controls.Add(this.buttonAdd);
+            this.tabPage2.Location = new System.Drawing.Point(4, 4);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(410, 209);
+            this.tabPage2.Size = new System.Drawing.Size(409, 160);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(413, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // языкToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.русскийToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "языкToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            this.languageToolStripMenuItem.Text = null;
+            // 
+            // русскийToolStripMenuItem
+            // 
+            this.русскийToolStripMenuItem.Name = "русскийToolStripMenuItem";
+            this.русскийToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.русскийToolStripMenuItem.Text = "Русский";
+            this.русскийToolStripMenuItem.Click += new System.EventHandler(this.RussianToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.EnglishToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            this.helpToolStripMenuItem.Text = null;
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.HelpToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 230);
+            this.ClientSize = new System.Drawing.Size(413, 211);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -190,23 +211,38 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.buttonAdd.Text = TranslateRusEng.Russian["Add"];
+            this.buttonDelete.Text = TranslateRusEng.Russian["Delete"];
+            this.tabPage1.Text = TranslateRusEng.Russian["Main Tab"];
+            this.bPredict.Text = TranslateRusEng.Russian["Predict"];
+            this.tabPage2.Text = TranslateRusEng.Russian["Edit"];
+            this.languageToolStripMenuItem.Text = TranslateRusEng.Russian["Language"];
+            this.helpToolStripMenuItem.Text = TranslateRusEng.Russian["Help"];
+            this.textBox1.Text = TranslateRusEng.Russian["Initial Text"];
 
         }
 
         #endregion
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button bSpravka;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button bAdd;
-        private System.Windows.Forms.Button bDelete;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button bPredict;
         private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem русскийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Visioner/Form1.cs
+++ Visioner/Form1.cs
@@ -1,31 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-using Visioner.Service;
+using Visioner.IO;
+using Visioner.Dictionary;
 
 namespace Visioner
 {
     public partial class Form1 : Form
     {
         private Random _random = new Random();
-        private BindingList<string> _cases;
+        private BindingList<string> _predictions;
         private readonly string path = $"{Environment.CurrentDirectory}\\Cases.json";
         private FileIOService _fileIOService;
+
+        string language = "rus";
 
         public Form1()
         {
             InitializeComponent();
-            _cases = new BindingList<string>();
+            _predictions = new BindingList<string>();
             _fileIOService = new FileIOService(path);
             LoadCases();
-
         }
 
         private void UpdateProgressBar(int i)
@@ -44,10 +41,34 @@ namespace Visioner
             
         }
         
-        private async void bPredict_Click(object sender, EventArgs e)
+        private async void ButtonPredict_Click(object sender, EventArgs e)
         {
+            if (_predictions.Count == 0)
+            {
+                if (language == "rus")
+                {
+                    textBox1.Text = TranslateRusEng.Russian["Empty predicts"]; ;
+                }
+                else
+                {
+                    textBox1.Text = TranslateRusEng.English["Empty predicts"];
+                }
+                return;
+            }
+
             bPredict.Enabled = false;
-            textBox1.Text = ("Гадаю...");
+
+            if (language == "rus")
+            {
+                textBox1.Text = TranslateRusEng.Russian["Predict."]; ;
+            }
+            else
+            {
+                textBox1.Text = TranslateRusEng.English["Predict."];
+            }
+
+            progressBar1.Focus();
+
             await Task.Run(() =>
             {
                 
@@ -61,45 +82,90 @@ namespace Visioner
                 }
 
             });
-            textBox1.Text = ("");
-            int rnd = _random.Next(_cases.Count);
-            MessageBox.Show(_cases[rnd].ToString());
 
+            int rnd = _random.Next(_predictions.Count);
+            textBox1.Text = _predictions[rnd];
+
+            progressBar1.Value = 0;
             bPredict.Enabled = true;
-        }
-
-
-        private void buttonSpravka_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Хули тут непонятного? Тыкаешь на кнопку, прога предсказывает твоё будущее. Не злоупотреблять, отвалятся яйца. Если ты девочка, отвалятся твои девчачьи яйца!");
         }
 
         private void LoadCases()
         {
             try
             {
-                _cases = _fileIOService.LoadData();
-                listBox1.DataSource = _cases;
+                _predictions = _fileIOService.LoadData();
+                listBox1.DataSource = _predictions;
             }
-            catch (Exception ex)
+            catch { }
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            try
             {
-                MessageBox.Show(ex.Message);
-                Close();
+                string newCases = textBox2.Text;
+                if (newCases != "")
+                {
+                    _predictions.Add(newCases);
+                    _fileIOService.SaveData(_predictions);
+                    textBox2.Clear();
+                }                
+            }
+            catch { }
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int indexOfListBox = listBox1.SelectedIndex;
+                _predictions.RemoveAt(indexOfListBox);
+                _fileIOService.SaveData(_predictions);
+
+            }
+            catch { }   
+        }
+
+        private void HelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (language == "rus")
+            {
+                MessageBox.Show(TranslateRusEng.Russian["Help Message"]);
+            }
+            else
+            {
+                MessageBox.Show(TranslateRusEng.English["Help Message"]);
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void RussianToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string newCases = textBox2.Text;
-            _cases.Add(newCases);
-            _fileIOService.SaveData(_cases);
-            textBox2.Clear();
+            language = "rus";
+            textBox1.Text = TranslateRusEng.Russian["Initial Text"];
+
+            bPredict.Text = TranslateRusEng.TranslateRus("Predict");
+            buttonAdd.Text = TranslateRusEng.TranslateRus("Add");
+            buttonDelete.Text = TranslateRusEng.TranslateRus("Delete");
+            tabPage1.Text = TranslateRusEng.TranslateRus("Main Tab");
+            tabPage2.Text = TranslateRusEng.TranslateRus("Edit");
+            languageToolStripMenuItem.Text = TranslateRusEng.TranslateRus("Language");
+            helpToolStripMenuItem.Text = TranslateRusEng.TranslateRus("Help");
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void EnglishToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            int indexOfListBox = listBox1.SelectedIndex;
-            _cases.RemoveAt(indexOfListBox);
+            language = "eng";
+            textBox1.Text = TranslateRusEng.English["Initial Text"];
+
+            bPredict.Text = TranslateRusEng.TranslateEng("Predict");
+            buttonAdd.Text = TranslateRusEng.TranslateEng("Add");
+            buttonDelete.Text = TranslateRusEng.TranslateEng("Delete");
+            tabPage1.Text = TranslateRusEng.TranslateEng("Main Tab");
+            tabPage2.Text = TranslateRusEng.TranslateEng("Edit");
+            languageToolStripMenuItem.Text = TranslateRusEng.TranslateEng("Language");
+            helpToolStripMenuItem.Text = TranslateRusEng.TranslateEng("Help");
         }
+
     }
 }
